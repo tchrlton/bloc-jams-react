26e50c01e9d6db43c1f4e971791e366c2868d90a
Thomas Charlton <tchrlton1@gmail.com>
2018-08-15 14:42:18 -0700

    deleted boilerplate code

--- src/App.js
+++ src/App.js
@@ -21,9 +21,6 @@ class App extends Component {
           <Route path="/library" component={Library} />
           <Route path="/album" component={Album} />
         </main>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
