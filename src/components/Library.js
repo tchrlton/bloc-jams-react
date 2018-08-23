3de8273c83c1ae96bc0e4e643e9c8e9783430ea8
Thomas Charlton <tchrlton1@gmail.com>
2018-08-22 17:43:21 -0700

    Updated Library Component Link

--- src/components/Library.js
+++ src/components/Library.js
@@ -14,7 +14,7 @@ class Library extends Component {
       <section className='Library'>
         {
           this.state.albums.map( (album, index) =>
-            <Link to={'/album/${album.slug}'} key={index}>
+            <Link to={`/album/${album.slug}`} key={index}>
               <img src={album.albumCover} alt={album.title} />
               <div>{album.title}</div>
               <div>{album.artist}</div>
